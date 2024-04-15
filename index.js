c0d65c5f002e0d190c3bc45e674ea39af90918ff
walterayiego <walterayiego@gmail.com>
2024-04-15 19:43:00 +0300

    db setup

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { run } = require("./db");
 const app = express();
 const port = 3000;
 
@@ -9,3 +10,5 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+run().catch(console.dir);
