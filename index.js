dcf983cc330a3a4eff1733cde38479c6eef8fc90
walterayiego <walterayiego@gmail.com>
2024-04-22 10:51:32 +0300

    Refactor MongoDB connection and add API endpoints for creating and retrieving products

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
 const port = 3000;
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/api/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(201).json(product);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 mongoose
