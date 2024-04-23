4e16c4e32a7fa513fdedf7c5605ea36cec1554ab
walterayiego <walterayiego@gmail.com>
2024-04-23 18:42:37 +0300

    Updated PUT Api

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.get("/api/product/:id", async (req, res) => {
   }
 });
 
+// POST DATA TO DB THROUGH THIS API
 app.post("/api/products", async (req, res) => {
   try {
     const product = await Product.create(req.body);
@@ -27,6 +28,29 @@ app.post("/api/products", async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+// Update Data
+app.put("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+
+    // Check for errors before sending updated product
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    console.log(product);
+
+    // res.status(404).json({ message: "Product not found" });
+    
+    const updatedProduct = await Product.findById(id);
+    
+    res.status(200).json(updatedProduct);
+    // Send updated product to the client
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error", error: error });
   }
 });
 
